ff1637b4389e7376a8c5e47ed60221c2bcc358b2
huseynmohameda <huseynmohamedabdi@gmail.com>
2026-09-22 03:07:07 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,502 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception",
+      "name": "RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.673259167499296,
+          "pitch": -0.046241214254630236,
+          "rotation": 0,
+          "target": "4-iv-room"
+        },
+        {
+          "yaw": 0.6033409505378167,
+          "pitch": -0.017896075780143406,
+          "rotation": 0,
+          "target": "2-retail"
+        },
+        {
+          "yaw": 1.0402914145130264,
+          "pitch": 0.021206479674861,
+          "rotation": 0,
+          "target": "1-view-angel-reception"
+        },
+        {
+          "yaw": 1.2619167773265314,
+          "pitch": -0.04703522149656436,
+          "rotation": 0,
+          "target": "9-lab"
+        },
+        {
+          "yaw": -1.1105687443251,
+          "pitch": -0.004556145929448974,
+          "rotation": 0,
+          "target": "8-change-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-view-angel-reception",
+      "name": "VIEW ANGEL RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.456983383573748,
+          "pitch": -0.18526379466836929,
+          "rotation": 0,
+          "target": "9-lab"
+        },
+        {
+          "yaw": 0.6548446629982028,
+          "pitch": 0.1573609967072116,
+          "rotation": 0,
+          "target": "2-retail"
+        },
+        {
+          "yaw": -0.33124791871824044,
+          "pitch": -0.02869818222638898,
+          "rotation": 0,
+          "target": "4-iv-room"
+        },
+        {
+          "yaw": -0.46820688853692616,
+          "pitch": 0.03863796950128773,
+          "rotation": 0,
+          "target": "3-corridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-retail",
+      "name": "RETAIL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-corridor",
+      "name": "CORRIDOR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5314955838389039,
+          "pitch": -0.05973382760964796,
+          "rotation": 0,
+          "target": "8-change-room"
+        },
+        {
+          "yaw": 0.21231323784820333,
+          "pitch": -0.1526766570668947,
+          "rotation": 0,
+          "target": "7-prp-room"
+        },
+        {
+          "yaw": -0.011973224458433407,
+          "pitch": 0.012047273426535554,
+          "rotation": 4.71238898038469,
+          "target": "6-conceltation-room"
+        },
+        {
+          "yaw": -1.0353783699942944,
+          "pitch": -0.07947511613866887,
+          "rotation": 0,
+          "target": "5-head-wash-rom"
+        },
+        {
+          "yaw": -2.5786888390394616,
+          "pitch": 0.03381966091641431,
+          "rotation": 0,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-iv-room",
+      "name": "IV ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-head-wash-rom",
+      "name": "HEAD WASH ROM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-conceltation-room",
+      "name": "CONCELTATION ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-prp-room",
+      "name": "PRP ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-change-room",
+      "name": "CHANGE ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-lab",
+      "name": "LAB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.005517211714941794,
+          "pitch": 0.04280452561715009,
+          "rotation": 1.5707963267948966,
+          "target": "1-view-angel-reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-red-zone",
+      "name": "RED ZONE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10880144659823188,
+          "pitch": -0.07044436785846031,
+          "rotation": 0,
+          "target": "11-hair-shaving-"
+        },
+        {
+          "yaw": -0.657734249029728,
+          "pitch": 0.04352893685221915,
+          "rotation": 0,
+          "target": "12-sterlization"
+        },
+        {
+          "yaw": 0.8569543137716913,
+          "pitch": -0.016213734351826048,
+          "rotation": 0,
+          "target": "13-ot-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-hair-shaving-",
+      "name": "HAIR SHAVING ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-sterlization",
+      "name": "STERLIZATION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-ot-room",
+      "name": "OT ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GERWEYNE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
